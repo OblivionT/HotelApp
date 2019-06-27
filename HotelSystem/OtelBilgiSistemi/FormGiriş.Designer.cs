fc3c315f6f3b924cde920ebe82cc9af3fe2426b6
Berke Öncül <berkeoncul4@gmail.com>
2019-06-27 17:44:01 +0300

    Add files via upload

---
+++ HotelSystem/OtelBilgiSistemi/FormGiriş.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace OtelBilgiSistemi
+{
+    partial class FormGiriş
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGiriş));
+            this.txtKullanıcıAdı = new System.Windows.Forms.TextBox();
+            this.txtŞifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGiriş = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKullanıcıAdı
+            // 
+            this.txtKullanıcıAdı.Location = new System.Drawing.Point(99, 80);
+            this.txtKullanıcıAdı.MaxLength = 20;
+            this.txtKullanıcıAdı.Name = "txtKullanıcıAdı";
+            this.txtKullanıcıAdı.Size = new System.Drawing.Size(132, 20);
+            this.txtKullanıcıAdı.TabIndex = 0;
+            // 
+            // txtŞifre
+            // 
+            this.txtŞifre.Location = new System.Drawing.Point(99, 149);
+            this.txtŞifre.MaxLength = 15;
+            this.txtŞifre.Name = "txtŞifre";
+            this.txtŞifre.PasswordChar = '•';
+            this.txtŞifre.Size = new System.Drawing.Size(132, 20);
+            this.txtŞifre.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Şifre";
+            // 
+            // btnGiriş
+            // 
+            this.btnGiriş.Image = ((System.Drawing.Image)(resources.GetObject("btnGiriş.Image")));
+            this.btnGiriş.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGiriş.Location = new System.Drawing.Point(99, 199);
+            this.btnGiriş.Name = "btnGiriş";
+            this.btnGiriş.Size = new System.Drawing.Size(132, 33);
+            this.btnGiriş.TabIndex = 19;
+            this.btnGiriş.Text = "Giriş";
+            this.btnGiriş.UseVisualStyleBackColor = true;
+            this.btnGiriş.Click += new System.EventHandler(this.btnGiriş_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(323, 55);
+            this.panel1.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(135, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Giriş";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(220, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormGiriş
+            // 
+            this.AcceptButton = this.btnGiriş;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 282);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGiriş);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtŞifre);
+            this.Controls.Add(this.txtKullanıcıAdı);
+            this.Name = "FormGiriş";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Otel Bilgi Sistemi";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKullanıcıAdı;
+        private System.Windows.Forms.TextBox txtŞifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGiriş;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
